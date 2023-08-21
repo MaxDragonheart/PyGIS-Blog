9a9439bbc19c783f3cb3e5a8e14a963aac115847
MaxDragonheart <massimilianomoraca@gmail.com>
2023-08-21 12:38:12 +0200

    #16

--- notebooks/2023/08/21/analisys.py
+++ notebooks/2023/08/21/analisys.py
@@ -25,29 +25,34 @@ regions = main_folder.joinpath('Reg01012023_g').joinpath('Reg01012023_g_WGS84.sh
 regions_gdf = gpd.read_file(regions).to_crs(osm_crs)
 italia = regions_gdf.dissolve().squeeze()
 
-italia_data = ox.features_from_polygon(
-    polygon=italia.geometry,
-    tags={'historic': True}
-)
+# italia_data = ox.features_from_polygon(
+#     polygon=italia.geometry,
+#     tags={'historic': True}
+# )
+#
+# italia_data = italia_data[['historic', 'name', 'historic:civilization', 'geometry']]
+# italia_data = italia_data[~italia_data['historic'].isin(excluded_keys)]
+# italia_data = italia_data[italia_data['name'].notna()]
+# italia_data.reset_index(inplace=True)
+#
+# italia_polygons = italia_data[italia_data['element_type'] == 'way']
+#
+# italia_final = pd.concat([italia_polygons, italia_data[italia_data['element_type'] == 'node']])
+# italia_final.drop(columns={'element_type'}, inplace=True)
+# italia_final.sort_values(by='name', inplace=True)
+# italia_final = gpd.GeoDataFrame(italia_final, geometry='geometry')
 
-italia_data = italia_data[['historic', 'name', 'historic:civilization', 'geometry']]
-italia_data = italia_data[~italia_data['historic'].isin(excluded_keys)]
-italia_data = italia_data[italia_data['name'].notna()]
-italia_data.reset_index(inplace=True)
-
-italia_polygons = italia_data[italia_data['element_type'] == 'way']
-
-italia_final = pd.concat([italia_polygons, italia_data[italia_data['element_type'] == 'node']])
-italia_final.drop(columns={'element_type'}, inplace=True)
-italia_final.sort_values(by='name', inplace=True)
-italia_final = gpd.GeoDataFrame(italia_final, geometry='geometry')
+italia_final = regions_gdf
+output_file = Path.cwd().joinpath('archeological_site.gpkg')
+italia_final.to_file('archeological_site.gpkg', driver='GPKG', layer='italy')
 
 
 if __name__ == '__main__':
     time_start = datetime.datetime.now()
     logging.info(f'Start analysis at {time_start}')
 
-    print(italia)
+    logging.info(f"Total sites: {len(italia_final.index)}")
+    logging.info(f"Saved to {str(output_file)}")
 
     time_end = datetime.datetime.now()
     time_diff = time_end - time_start
